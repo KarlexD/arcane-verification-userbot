04cc6f9ebf28ed39f6d4ed4dd6bf719889cadb7a
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-02-15 09:23:48 +0530

    Create newmember.py

---
+++ arcane/plugins/newmember.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from .. import bot, ARCANEUSER
+from pyrogram.types import Message
+
+
+@bot.on_message(filters.new_chat_members)
+async def arcane(_, m: Message):
+    user = m.from_user.id
+    if user in ARCANEUSER:
+        await m.reply_text(
+            f"**Welcome The Arcane [{m.from_user.first_name}-Kun](tg://user?id={user}) Member**"
+        )
+    else:
+        pass
