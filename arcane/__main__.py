7179ec13ac45c7cde37333cc321a8ab5da9ff760
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-02-15 09:19:15 +0530

    Create __main__.py

---
+++ arcane/__main__.py
@@ -0,0 +1,23 @@
+from . import bot
+import logging
+from app import ldb
+import logging
+
+logging.basicConfig(
+    filename="logs.txt",
+    level=logging.INFO,
+    filemode="a",
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+
+
+def main():
+    ldb.create_db()
+    bot.run()
+    # app.run()
+
+
+if __name__ == "__main__":
+    main()
