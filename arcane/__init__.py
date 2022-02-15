dcc726b5d74f6c243349b8906ba3454f89378a93
Hell God <83143204+kari-36@users.noreply.github.com>
2022-02-15 14:13:40 +0500

    Update __init__.py

--- arcane/__init__.py
+++ arcane/__init__.py
@@ -1,7 +1,12 @@
 from pyrogram import Client
 import os
 
+ARCANEUSER = os.environ.get("ARCANEUSER", None)
+SESSION = os.environ.get("SESSION", None)
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
 
+"""
 ARCANEUSER = [
     5086015489, 5086015489
 ]
@@ -9,6 +14,8 @@ ARCANEUSER = [
 SESSION = ""
 API_ID = "1998138"
 API_HASH = "13862d4eadf08465eef9f56470d1ba10"
+"""
+
 
 bot = Client(session_name=SESSION,
              api_id=API_ID,
